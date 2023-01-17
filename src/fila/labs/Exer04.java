c59e76f66032aa659a2e863ae8e09ad26041fc29
Coimbra <adsjkbd@gmail.com>
2023-01-17 10:04:11 -0300

    Filas: Exer 04 Brincadeira Batata Quente

---
+++ src/fila/labs/Exer04.java
@@ -0,0 +1,34 @@
+package fila.labs;
+
+import fila.Fila;
+import fila.FilaComPrioridade;
+
+import java.util.Random;
+
+public class Exer04 {
+
+    public static void main(String[] args) {
+
+        Fila<Integer> fila = new Fila<>();
+
+        for (int i = 0; i < 10; i++) {
+            fila.enfileirar(i);
+        }
+
+        Random aleatorio = new Random();
+        int num = 0;
+        while (num == 0) {
+            num = aleatorio.nextInt(10);
+        }
+
+        System.out.println("Número = " + num);
+
+        while (fila.tamanho() > 1) {
+            for (int i = 0; i < num; i++) {
+                fila.enfileirar(fila.desenfileirar());
+            }
+            System.out.println("Eliminado = " + fila.desenfileirar());
+        }
+        System.out.println("***Ganhador**** > " + fila.desenfileirar());
+    }
+}
